d2989630368cf688358553d860e5f11f77dcd427
Lowry Nakamura <lowry.nakamura@gmail.com>
2019-05-16 17:41:45 -0700

    latest state of code

--- js/app.js
+++ js/app.js
@@ -19,11 +19,28 @@ $(start).on('click', function () {
 
 //get random phrases
 function getRandomPhraseAsArray(arr) {
-	var randomPhrase = [Math.floor(Math.random()*phrases.length)]
-	console.log(randomPhrase);
+	//remove decimals from the array selection
+	var randomPhrase = [Math.floor(Math.random()*phrases.length)];
+	//select an array from the array random Phrases
+	var selectPhrase = arr[randomPhrase]
+	//split the array into a new array of characters
+	var splitArr = selectPhrase.split('')
+	return(splitArr)
 }
 
 //call functions 
 getRandomPhraseAsArray(phrases);
 
+//set game display
 
+function addPhraseToDisplay(arr){
+	var newCharacter = splitArr;
+    for (var i =0; i < newCharacter.length; i += 0) {
+    	console.log('this kinda works');
+    }
+}
+/*
+Create an addPhraseToDisplay function that loops through an array of characters. Inside the loop, for each character in the array, you’ll create a list item, put the character inside of the list item, and append that list item to the #phrase ul in your HTML. If the character in the array is a letter and not a space, the function should add the class “letter” to the list item.
+You’ll need to write the addPhraseToDisplay function so that it can take any array of letters and add it to the display. To do that, the function will need to take an array as a parameter:
+
+*/
