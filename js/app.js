dc03cd8eac86ca67b47593bf92752763ee4be1ff
Lowry Nakamura <lowry.nakamura@gmail.com>
2019-05-31 22:39:17 -0700

    game complete

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-var qwerty = document.getElementById('#qwerty');
-var phrase = document.getElementById('#phrase');
+var qwerty = document.getElementById('qwerty');
+var phrase = document.getElementById('phrase');
 
 var missed = 0;
 var start = document.querySelector('.btn__reset');
@@ -11,15 +11,39 @@ var phrases = [
 	'I need to learn Javascript',
 	'I am tired'
 ];
+function setLifeCounter() {
+	document.querySelectorAll('.tries img').forEach(function(t, i) {
+			if (i < missed) {
+				t.setAttribute('src', 'images/lostHeart.png')
+			} else {
+				t.setAttribute('src', 'images/liveHeart.png')
+			}
+	});
+}
+function reset() {
+	document.querySelectorAll('.letter').forEach(function(r) {
+		r.remove();
+	});
+	document.querySelectorAll('.space').forEach(function(r) {
+		r.remove();
+	});
 
-// click to hide overlay
-$(start).on('click', function () {
-	$('#overlay').hide();
+	document.getElementById('overlay').style.display = 'none';
 	var randomPhrase = getRandomPhraseAsArray(phrases);
 	addPhraseToDisplay(randomPhrase);
 
-});
-
+	qwerty.querySelectorAll('button').forEach(function(b){
+		b.classList.remove('chosen')
+		b.removeAttribute('disabled');
+		document.querySelectorAll('.letter').forEach(function(s) {
+			s.classList.remove('show');
+		});
+	});
+	missed = 0;
+	setLifeCounter();
+}
+// click to hide overlay
+start.onclick = reset;
 //get random phrases
 function getRandomPhraseAsArray(arr) {
 	//remove decimals from the array selection
@@ -40,26 +64,50 @@ function addPhraseToDisplay(arr){
 	for(var i = 0; i < arr.length; i += 1) {
 		var character = document.createElement('li');
 		character.innerHTML = arr[i];
-		character.classList.add('letter');
+		if (arr[i] == ' ') {
+			character.classList.add('space');
+		} else {
+			character.classList.add('letter');
+		};
 		arr[i];
 		document.getElementById('phrase').children[0].appendChild(character);
-	}	
-}
-
+	};
+};
 function checkLetter(button) {
 	var letters = document.getElementsByClassName('letter');
 	var match = null;
 	for(var i = 0; i < letters.length; i += 1) {
-		if (button.innerHTML == letters[i].innerHTML) {
+		if (button.innerHTML.toLowerCase() == letters[i].innerHTML.toLowerCase()) {
 			letters[i].classList.add('show');
 			match = letters[i];
 		} 
 	}
 	return(match); 
 }
-
-window.addEventListener("keydown", keysPressed, false);
-
-function keysPressed(e) {
-	alert('this works');
-}
+qwerty.querySelectorAll('button').forEach(function(b){
+	b.onclick = function () {
+		this.setAttribute('disabled', true);
+		this.classList.add('chosen');
+		var letterFound = checkLetter(this);
+		if (letterFound == null) {
+			missed++;
+		} 
+		setLifeCounter();
+		if (missed >= 5) {
+			document.getElementById('overlay').style.display = 'block';
+			document.querySelector('#overlay').className = 'lose';
+			document.querySelector('.btn__reset').innerHTML = 'Restart Game';
+		} 
+		var lettersRemaining = 0;
+		document.querySelectorAll('.letter').forEach(function(l) {
+			if (!l.classList.contains('show')) {
+				lettersRemaining++;
+			}
+		});
+		if (lettersRemaining === 0) {
+			document.getElementById('overlay').style.display = 'block';
+			document.querySelector('#overlay').className = 'win';
+			document.querySelector('.btn__reset').innerHTML = 'Restart Game';
+		}
+	}
+})
